30013a7c0175444d3975eabb277d4b37077b0498
SirishaBathina <141664760+SirishaBathina@users.noreply.github.com>
2023-08-07 22:52:22 +0100

    Update gender.sh

--- gender.sh
+++ gender.sh
@@ -1,11 +1,20 @@
-def get_honorific(age, gender):
-    if age < 18:
-        return "Young " + ("lady" if gender == "female" else "man")
-    elif age >= 18 and age <= 60:
-        return "Mr." if gender == "male" else "Ms."
-    else:
-        return "Sir" if gender == "male" else "Madam"
-age = int(input("Enter the person's age: "))
-gender = input("Enter the person's gender (male/female): ").lower()
-honorific = get_honorific(age, gender)
-print("The honorific to use when addressing the person is:", honorific)
+#!/bin/bash
+read -p "Enter person's age: " age
+read -p "Enter person's gender (male/female): " gender
+honorific=""
+if [ "$age" -lt 18 ]; then
+    honorific="Young"
+elif [ "$age" -ge 18 ] && [ "$age" -lt 50 ]; then
+    if [ "$gender" == "male" ]; then
+        honorific="Mr."
+    else
+        honorific="Ms."
+    fi
+else
+    if [ "$gender" == "male" ]; then
+        honorific="Sir"
+    else
+        honorific="Madam"
+    fi
+fi
+echo "Dear $honorific, ..."
